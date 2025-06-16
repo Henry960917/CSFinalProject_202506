b0e3d8217855a0e8077a20d5febebbb460af082e
Henry960917 <148993233+Henry960917@users.noreply.github.com>
2025-06-16 15:29:01 +0800

    Update numOfSAW.cpp

--- numOfSAW.cpp
+++ numOfSAW.cpp
@@ -1,37 +1,37 @@
-//#include <iostream>
-//#include <vector>
-//#include <climits>
-//using namespace std;
-//
-//int dx[] = { -1, 0, 1, 0 };
-//int dy[] = { 0, 1, 0, -1 };
-//int cnt = 0;
-//const int N = 100;
-//vector<vector<bool>> visited(N, vector<bool>(N));
-//
-//void dfs(int x, int y, int stepsRemain) {
-//	if(stepsRemain == 0) {
-//		cnt++;
-//		return;
-//	}
-//	for (int i = 0; i < 4; i++) {
-//		int nx = x + dx[i];
-//		int ny = y + dy[i];
-//		if (!visited[nx][ny]) {
-//			visited[nx][ny] = true;
-//			dfs(nx, ny, stepsRemain - 1);
-//			visited[nx][ny] = false; // ¦^·¹
-//		}
-//	}
-//}
-//
-//int main() {
-//	cout << "½Ğ¿é¤J¨B¼Æ(¥²¶·¬O¥¿¾ã¼Æ, ¥B¤p©ó22): ";
-//	int n;
-//	cin >> n;
-//	int start = N / 2;
-//	visited[start][start] = true;
-//	dfs(start, start, n);
-//	cout << "¦ÛÁ×¦æ¨«Á`¦@¦³ " << cnt << " ºØ¨«ªk" << endl;
-//	return 0;
-//}
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int dx[] = { -1, 0, 1, 0 };
+int dy[] = { 0, 1, 0, -1 };
+int cnt = 0;
+const int N = 100;
+vector<vector<bool>> visited(N, vector<bool>(N));
+
+void dfs(int x, int y, int stepsRemain) {
+	if(stepsRemain == 0) {
+		cnt++;
+		return;
+	}
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if (!visited[nx][ny]) {
+			visited[nx][ny] = true;
+			dfs(nx, ny, stepsRemain - 1);
+			visited[nx][ny] = false; // å›æº¯
+		}
+	}
+}
+
+int main() {
+	cout << "è«‹è¼¸å…¥æ­¥æ•¸(å¿…é ˆæ˜¯æ­£æ•´æ•¸, ä¸”å°æ–¼22): ";
+	int n;
+	cin >> n;
+	int start = N / 2;
+	visited[start][start] = true;
+	dfs(start, start, n);
+	cout << "è‡ªé¿è¡Œèµ°ç¸½å…±æœ‰ " << cnt << " ç¨®èµ°æ³•" << endl;
+	return 0;
+}
